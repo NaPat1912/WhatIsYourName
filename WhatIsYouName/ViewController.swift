cf270cb65a374b231b5fa43bca7111eb58cfe143
test <asdfg@mail.com>
2018-03-10 09:48:18 +0700

    Initial Commit

---
+++ WhatIsYouName/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatIsYouName
+//
+//  Created by nb1's MacBookPro on 10/3/2561 BE.
+//  Copyright © 2561 napat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }// main function คือfuncแรกที่เริ่มจะทำงาน
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }//นี่คือส่วนของการจัดการหน่วยความจำ
+
+
+}//main class
+
